0cf3d7643101db0010d3ff3f76af48a40fe26fbd
pluto <562438145@qq.com>
2018-06-23 00:52:18 +0800

    不知道更新了什么

--- src/main/java/com/eleme/web/PayController.java
+++ src/main/java/com/eleme/web/PayController.java
@@ -2,48 +2,73 @@ package com.eleme.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.eleme.dao.PayRecordDao;
+import com.eleme.dao.UserDao;
 import com.eleme.entity.Pay;
-import com.eleme.service.StatService;
 import com.eleme.util.PayUtil;
+
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller  
 public class PayController {  
 	
 	@Autowired
-	private StatService statService;
+	private UserDao userDao;
+	@Autowired
+	private PayRecordDao payRecordDao;
 
     //
 	@RequestMapping(value = "/pay",method = RequestMethod.POST)
 	@ResponseBody
 	public Map<String, Object> pay(@RequestParam (value="price", required = false)float price,
-			@RequestParam (value="istype", required = false)int istype) throws Exception {
-		System.out.println(price);
+			@RequestParam (value="istype", required = false)int istype,
+		@RequestParam (value="orderuid", required = false)String orderuid) throws Exception {
 		Map<String, Object> resultMap = new HashMap<String, Object>();
 		Map<String, Object> remoteMap = new HashMap<String, Object>();
 		remoteMap.put("price", price+"");
 		remoteMap.put("istype", istype);
 		remoteMap.put("orderid", PayUtil.getOrderIdByUUId());
-		remoteMap.put("orderuid", null);
+		remoteMap.put("orderuid", orderuid);
 		remoteMap.put("goodsname", null);
 		resultMap.put("data", PayUtil.payOrder(remoteMap));
 		return resultMap;
     }
 
-	@RequestMapping(value="/notifyPay",method = RequestMethod.POST)
-	@ResponseBody
-	public void notifyPay(@RequestParam (value="pay", required = false) Pay pay) throws Exception {
+	@RequestMapping("/notifyPay")
+	public void notifyPay(HttpServletRequest request, HttpServletResponse response, Pay pay) throws Exception {
 		// 保证密钥一致性
 		if (PayUtil.checkPayKey(pay)) {
-			System.out.println("支付成功");
-		} else {
-			// TODO 该怎么做就怎么做
+			if(userDao.checkUser(pay.getOrderuid()) == null){
+				userDao.insertUser(pay.getOrderuid());
+			}
+			userDao.addBalance(pay.getOrderuid(), pay.getPrice());
+			Date time= new java.sql.Timestamp(new java.util.Date().getTime());
+			payRecordDao.insertPayRecord(pay.getPaysapi_id(),pay.getOrderid(), pay.getPrice(), pay.getRealprice(), pay.getOrderuid(),time,1);
+		}else{
+			Date time= new java.sql.Timestamp(new java.util.Date().getTime());
+			payRecordDao.insertPayRecord(pay.getPaysapi_id(),pay.getOrderid(), pay.getPrice(), pay.getRealprice(), pay.getOrderuid(),time,0);
 		}
 	}
+	
+	//获取记录
+		@RequestMapping(value = "/checkBalance",method = RequestMethod.POST)
+		@ResponseBody
+	    public int checkBalance(@RequestParam (value="phone", required = false)String phone) throws Exception {
+			if(userDao.checkUser(phone) == null){
+				return 0;
+			}else{
+				return userDao.getBalance(phone);  
+			}
+	    }
 }
